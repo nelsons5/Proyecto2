3cb5e656d2ee452800515584dc43e9fa131981e3
unknown <ncifuentesc@miumg.edu.gt>
2020-09-28 23:48:05 -0600

    creacion del proyecto en spring

---
+++ pr2/pr2/src/main/java/proyecto2/pr2/Proyecto2Application.java
@@ -0,0 +1,13 @@
+package proyecto2.pr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2Application.class, args);
+	}
+
+}
---
+++ pr2/pr2/src/test/java/proyecto2/pr2/Proyecto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto2.pr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
